e41bdd1c4d7861c7321a65d54106fd469ff19ec6
Dumi Loghin <dumiloghin@gmail.com>
2019-02-19 09:17:16 +0000

    update clients and hosts files on each run

--- benchmark/ethereum/update-env.sh
+++ benchmark/ethereum/update-env.sh
@@ -4,10 +4,10 @@
 
 # copy the latest env.sh to nodes and clients
 for host in `cat $HOSTS`; do
-  scp -oStrictHostKeyChecking=no env.sh $host:$ETH_HOME/
+  scp -oStrictHostKeyChecking=no env.sh $HOSTS $CLIENTS $host:$ETH_HOME/
 done
 
 for host in `cat $CLIENTS`; do
-  scp -oStrictHostKeyChecking=no env.sh $host:$ETH_HOME/
+  scp -oStrictHostKeyChecking=no env.sh $HOSTS $CLIENTS $host:$ETH_HOME/
 done
 
