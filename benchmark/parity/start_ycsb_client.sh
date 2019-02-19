533a1a3a127246520b81b5a1d962727fde95b8ac
Dumi Loghin <dumiloghin@gmail.com>
2019-02-19 10:42:25 +0000

    parity benchmark sh executable

--- benchmark/parity/start_ycsb_client.sh
+++ benchmark/parity/start_ycsb_client.sh
@@ -3,6 +3,9 @@ THREADS=$1
 HOST=$2
 LOG=$3
 RATE=$4
+
+export LD_LIBRARY_PATH="/usr/local/lib"
+
 cd `dirname ${BASH_SOURCE-$0}`
 EXE="../../src/macro/kvstore/driver"
 nohup $EXE -db parity -threads $THREADS -P ../../src/macro/kvstore/workloads/workloada.spec -endpoint $HOST -txrate $RATE > $LOG 2>&1 &
