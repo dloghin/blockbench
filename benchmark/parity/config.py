771ea8a9325fd91c6651343edd9fc5fea53eeeb6
Dumi Loghin <dumiloghin@gmail.com>
2019-02-26 16:01:31 +0000

    config on xeon

--- benchmark/parity/config.py
+++ benchmark/parity/config.py
@@ -1,18 +1,20 @@
 NS=[8]
-NODES=['10.0.0.{}'.format(x) for x in range(3,36)]
+NODES=['10.0.0.{}'.format(x) for x in range(82,90)]
 
-CLIENTS=['10.0.0.{}'.format(x) for x in range(85,100)]
+CLIENTS=['10.0.0.{}'.format(x) for x in range(90,98)]
 PORT='8545'
 #THREADS=[2,4,8,16,32]
 #RATES=[1,2,4,8,12,16]
-THREADS=[32]
+THREADS=[8]
 RATES=[8]
+TIMETORUN=300
+DEPLOY_TIME=60
 
-HOME_DIR='/users/dinhtta/anh/blockchain-perf/benchmark/parity'
-CHAIN_DATA='/data/dinhtta/chain-data'
-LOG_DIR='/data/dinhtta/logs/parity'
-PARITY_EXE='/users/dinhtta/.cargo/bin/parity'
-CLIENT_LOG='logs_stepDuration_2'
+HOME_DIR='/data/dumi/blockbench/benchmark/parity'
+CHAIN_DATA='/tmp/chain-data'
+LOG_DIR='/data/dumi/blockbench/benchmark/parity/logs'
+PARITY_EXE='/users/dumi/git/parity-ethereum/target/release/parity'
+CLIENT_LOG='client_parity'
 
 CLIENT_DRIVER='start_ycsb_client.sh'
 
@@ -22,4 +24,4 @@ kill_command = 'ssh -o StrictHostKeyChecking=no dinhtta@{} "killall -KILL {}"'
 ssh_command = 'ssh -o StrictHostKeyChecking=no dinhtta@{} {}'
 
 partition_cmd = './partition.sh {} {} {} &'
-TIMEOUT=150
+TIMEOUT=100
