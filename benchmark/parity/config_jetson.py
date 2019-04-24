3b3053b53523eb3c6611916b8b1bb8255c9e4ec2
Dumi Loghin <dumiloghin@gmail.com>
2019-04-24 01:35:16 +0000

    parity hetro run

---
+++ benchmark/parity/config_jetson.py
@@ -0,0 +1,27 @@
+NS=[4]
+NODES=['192.168.100.{}'.format(x) for x in range(111,115)]
+
+CLIENTS=['192.168.100.{}'.format(x) for x in range(202,206)]
+PORT='8545'
+#THREADS=[2,4,8,16,32]
+#RATES=[1,2,4,8,12,16]
+THREADS=[8]
+RATES=[32]
+TIMETORUN=300
+DEPLOY_TIME=60
+
+HOME_DIR='/home/ubuntu/git/blockbench/benchmark/parity'
+CHAIN_DATA='/tmp/chain-data'
+LOG_DIR='/home/ubuntu/git/blockbench/benchmark/parity/logs'
+PARITY_EXE='/home/ubuntu/git/parity-ethereum/target/release/parity'
+CLIENT_LOG='client_parity'
+
+CLIENT_DRIVER='start_ycsb_client.sh'
+
+enode_command = 'curl --data \'{{"jsonrpc": "2.0", "method": "parity_enode", "params":[], "id": 0}}\' -H "Content-Type: application/json" -X POST {}:{}'
+add_peer = 'curl --data \'{{"jsonrpc": "2.0", "method": "parity_addReservedPeer", "params":["{}"], "id": 0}}\' -H "Content-Type: application/json" -X POST {}:{}'
+kill_command = 'ssh -o StrictHostKeyChecking=no {} "killall -KILL {}"'
+ssh_command = 'ssh -o StrictHostKeyChecking=no {} {}'
+
+partition_cmd = './partition.sh {} {} {} &'
+TIMEOUT=100
---
+++ benchmark/parity/run-hetero.py
@@ -0,0 +1,177 @@
+#!/bin/python
+import os
+import subprocess
+import sys
+from config_hetero import *
+from partition import partition
+import time
+import datetime
+
+MASTER_LOG_DIR=""
+
+def execute(cmd):
+  print(cmd)
+  os.system(cmd)
+
+def start_parity():
+  # generate scripts
+  os.system('python gen_config.py {}'.format(len(NODES)))
+
+  idx = 1
+  for node in NODESUSERS:
+    cmd = "scp chain_spec.json config.toml.{} {}:{}".format(idx, node, HOME_DIR)
+    execute(cmd)
+    idx = idx + 1
+
+  parity_script = '. {}/start_parity.sh {} {} {} {} {}'
+  count=1
+  for idx in range(len(NODES)):
+    cmd = ssh_command.format(NODESUSERS[idx], parity_script.format(HOME_DIR, CHAIN_DATA, NODES[idx], LOG_DIR, PARITY_EXE, count))
+    count = count +1
+    execute(cmd)
+
+def start_clients(threads,rate, log_dir, workload='ycsb'):
+  cs = zip(CLIENTS,NODES)
+  client_script = '. {}/start_{}_client.sh {} {}:{} {}/client_{}_{}_nodes_{}_threads_{}_rate {} {}'
+  for c in CLIENTS:
+    cmd = "ssh {} \"rm -rf {} && mkdir -p {}\"".format(c,LOG_DIR,LOG_DIR)
+    execute(cmd)
+  for (c,s) in cs:
+    cmd = ssh_command.format(c, client_script.format(HOME_DIR, workload, threads, s, PORT, LOG_DIR, s, len(NODES), threads, rate, rate, DEPLOY_TIME))
+    execute(cmd)
+
+def copy_logs():
+  global MASTER_LOG_DIR
+  if MASTER_LOG_DIR != "":
+    log_dir = MASTER_LOG_DIR
+  else:
+    log_dir = "parity-logs-{}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S'))
+  cmd = "mkdir {}".format(log_dir)
+  execute(cmd)
+  for idx in range(len(NODES)):
+    cms = "scp {}:{}/log {}/node-log-{}".format(NODESUSERS[idx], LOG_DIR, log_dir, NODES[idx])
+    execute(cms)
+  for c in CLIENTS:
+    cmd = "scp {}:{}/client* {}/".format(c, LOG_DIR, log_dir)
+    execute(cmd)
+
+def get_enodes():
+  results=[]
+  for node in NODES:
+     print enode_command.format(node,PORT)
+     p = os.popen(enode_command.format(node,PORT)).read()
+     p = p.split('"')
+     for l in p:
+      if l.startswith('enode'):
+        results.append(l)
+        break
+  print results
+  return results
+
+def add_peers():
+  enodes = get_enodes()
+  for node in NODES:
+    for en in enodes:
+      os.system(add_peer.format(en,node,PORT))
+
+def drop(nodes, n):
+  # drop the last n nodes:
+  for node in nodes[-n:]:
+    print "Dropping node {}".format(node)
+    os.system(kill_command.format(node, 'parity'))
+
+def kill():
+  for client in CLIENTS:
+    os.system(kill_command.format(client, 'driver'))
+  for node in NODESUSERS:
+    os.system(kill_command.format(node, 'parity'))
+
+def run_exp(log_file, threads, rates, sleep_time, is_security=False, dropping=False, workload='ycsb'):
+  print("Parity {} benchmark {} nodes {} clients {} threads {} txrate".format(workload, len(NODES), len(CLIENTS), threads, rates))
+  start_parity()
+  time.sleep(30)
+  add_peers()
+  time.sleep(60)
+  start_clients(threads, rates, log_file, workload)
+  if (not is_security) and (not dropping):
+    time.sleep(sleep_time)
+  else:
+    if is_security:
+      # for security part
+      time.sleep(100)
+      partition(NODES, TIMEOUT)
+      time.sleep(sleep_time-100-TIMEOUT)
+    else: # is dropping
+      time.sleep(250)
+      drop(NODES, 4)
+      time.sleep(sleep_time-250)
+  kill()
+  copy_logs()
+  time.sleep(5)
+
+def driver(is_security=False, is_fixed=False, is_drop=False, workload='ycsb'):
+  global NODES, CLIENTS #ugly hack
+  tmp_nodes = NODES
+  tmp_clients = CLIENTS 
+  if is_fixed or is_drop:
+    tmp_clients = CLIENTS[:8]
+
+  if '10.0.0.15' in tmp_nodes:
+    tmp_nodes.remove('10.0.0.15')
+
+  for n in NS: 
+    NODES = tmp_nodes[:n]
+    nclients = n
+    if is_fixed or is_drop:
+      nclients = 8
+
+    CLIENTS = tmp_clients[:(nclients/2)] + tmp_clients[:(nclients/2)]
+    print NODES
+    print CLIENTS
+    if sys.argv[1]=='start':
+      os.system('mkdir -p {}'.format(CLIENT_LOG))
+      for t in THREADS:
+        for r in RATES: 
+          run_exp(CLIENT_LOG, t, r, TIMETORUN, is_security, is_drop, workload)
+
+    elif sys.argv[1]=='kill':
+      kill()
+
+if __name__=='__main__':
+  error_msg = 'python run.py <start/kill> [-security/-fixed/-drop]\n'\
+            + '     start/kill:     start or kill the processes\n'\
+            + '                     with no othe argument, this will run the same\n'\
+            + '                     number of clients and servers\n'\
+            + '     -security:   run the security benchmark\n'\
+            + '     -fixed:      run the scale benchmark with fixed node\n'\
+            + '     -drop:       run the failure benchmark\n'\
+            + '     -smallbank:  run the smallbank benchmark\n'\
+            + '     -donothing:   run the donothing benchmark\n'
+  if len(sys.argv)<2:
+    print error_msg
+    sys.exit(1)
+
+  if len(sys.argv)>=4:
+    RATES=[sys.argv[3]]
+
+  if len(sys.argv)>=5:
+    MASTER_LOG_DIR=sys.argv[4]
+
+  if len(sys.argv)==2:
+    driver(False, False, False, 'ycsb')
+  elif sys.argv[2]=='-security':
+    driver(True, False, False, 'ycsb')
+  elif sys.argv[2]=='-fixed':
+    driver(False, True, False, 'ycsb')
+  elif sys.argv[2]=='-drop':
+    driver(False, False, True, 'ycsb')
+  elif sys.argv[2]=='-ycsb':
+    driver(False, False, False, 'ycsb')
+  elif sys.argv[2]=='-smallbank':
+    driver(False, False, False, 'smallbank')
+  elif sys.argv[2]=='-donothing':
+    driver(False, False, False, 'donothing')
+  else:
+    print error_msg
+    sys.exit(1)
+
